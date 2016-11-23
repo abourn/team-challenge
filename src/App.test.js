86e814c5d130ea055c2cfbc0d0df11490899684f
Andrew Joung <andrew.joung@gmail.com>
2016-11-23 09:48:23 -0800

    Import shallow from enzyme into . test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {shallow} from 'enzyme';
+
 describe('app will render', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
