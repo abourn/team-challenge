8d6f2661b4a89feb0df747c1b2b5ac9fd1b663c9
Adam Bourn <abourn@uw.edu>
2016-11-23 09:15:31 -0800

    add starter code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SignUpForm from './TeamSignUp';
+
+class App extends Component {
+  render() {
+    return (
+      <SignUpForm />
+    );
+  }
+}
+
+export default App;
